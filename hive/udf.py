fa53dfa016b5862b09d05007a214b1e78afbdc2b
Twein <twein1989@gmail.com>
2018-04-24 11:40:41 +0800

    ...

--- hive/udf.py
+++ hive/udf.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import sys
 
 for line in sys.stdin:
--- hive/udf.sql
+++ hive/udf.sql
@@ -11,3 +11,6 @@ insert into table u values('asdf4', 'asdf');
 
 hadoop fs -put udf.py /user/hive/lib/
 ADD FILE hdfs:///user/hive/lib/udf.py;
+
+SELECT TRANSFORM(fname, lname) USING 'udf.py' AS (fname, l_name) FROM u;
+SELECT TRANSFORM(fname, lname) USING 'python2.6 udf.py' AS (fname, l_name) FROM u;
