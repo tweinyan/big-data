261b4214da2626c210034896f6b94758923f703b
Twein <twein1989@gmail.com>
2018-05-29 15:38:43 +0800

    add regex

--- bigdataml/src/main/scala/myfirstScala.scala
+++ bigdataml/src/main/scala/myfirstScala.scala
@@ -1,60 +1,23 @@
 /**
   * Created by tweinyan on 03/05/2018.
   */
-import scala.util.Random
+
+import scala.util.matching.Regex
+
+
 object myfirstScala {
-  abstract class Notification
-
-  case class Email(sender: String, title: String, body: String) extends Notification
-  case class SMS(caller: String, message: String) extends  Notification
-  case class VoiceRecording(contactName: String, link: String) extends Notification
-
-  def showNotification(notification: Notification): String = {
-    notification match {
-      case Email(email, title, _) =>
-        s"You got an email from $email with title: $title"
-      case SMS(number, message) =>
-        s"You got an SMS from $number! Message: $message"
-      case VoiceRecording(name, link) =>
-        s"you received a Voice Recoding from $name! Click the link to hear it: $link"
-    }
-  }
-
-  def showImportantNotification(notification: Notification, importantPeopleInfo: Seq[String]): String = {
-    notification match {
-      case Email(email, _, _) if importantPeopleInfo.contains(email) =>
-        "You got an email from special someone!"
-      case SMS(number, _) if importantPeopleInfo.contains(number) =>
-        "You got an SMS from special someone!"
-      case other =>
-        showNotification(other)
-    }
-  }
-
   def main(args: Array[String]): Unit = {
-//    val someSms = SMS("12345", "Are you there?")
-//    val someVoiceRecoding = VoiceRecording("Tom", "voicerecoding.org/id/123")
-//    println(showNotification(someSms))
-//    println(showNotification(someVoiceRecoding))
-    val importantPeopleInfo = Seq("867-5309", "jenny@gmail.com")
-
-    val someSms = SMS("867-5309", "Are you there?")
-    val someVoiceRecording = VoiceRecording("Tom", "voicerecording.org/id/123")
-    val importantEmail = Email("jenny@gmail.com", "Drinks tonight?", "I'm free after 5!")
-    val importantSms = SMS("867-5309", "I'm here! Where are you?")
-
-    println(showImportantNotification(importantEmail, importantPeopleInfo))
-    println(showImportantNotification(someVoiceRecording, importantPeopleInfo))
-    println(showImportantNotification(importantEmail, importantPeopleInfo))
-    println(showImportantNotification(importantSms, importantPeopleInfo))
+    val keyValPattern: Regex = "([0-9a-zA-Z-#() ]+): ([0-9a-zA-Z-#() ]+)".r
+    val input: String =
+      """background-color: #A03300;
+        |background-image: url(img/header100.png);
+        |background-position: top center;
+        |background-repeat: repeat-x;
+        |background-size: 2160px 108px;
+        |margin: 0;
+        |height: 108px;
+        |width: 100%;""".stripMargin
+    for (patternMatch <- keyValPattern.findAllMatchIn(input))
+      println(s"key: ${patternMatch.group(1)} value: ${patternMatch.group(2)}")
   }
-  //def main(args: Array[String]): Unit = {
-  //  noData
-  //}
-  //def addInt(a:Int, b:Int):Int={
-  //  a + b
-  //}
-  //def noData:Unit={
-  //  println("no data")
-  //}
 }
