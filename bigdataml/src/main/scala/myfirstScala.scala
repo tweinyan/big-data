8cd18df9d0c12a79b37afb995a3256e98720e9e7
Twein <twein1989@gmail.com>
2018-05-29 14:11:41 +0800

    add pattern guards

--- bigdataml/src/main/scala/myfirstScala.scala
+++ bigdataml/src/main/scala/myfirstScala.scala
@@ -20,11 +20,33 @@ object myfirstScala {
     }
   }
 
+  def showImportantNotification(notification: Notification, importantPeopleInfo: Seq[String]): String = {
+    notification match {
+      case Email(email, _, _) if importantPeopleInfo.contains(email) =>
+        "You got an email from special someone!"
+      case SMS(number, _) if importantPeopleInfo.contains(number) =>
+        "You got an SMS from special someone!"
+      case other =>
+        showNotification(other)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    val someSms = SMS("12345", "Are you there?")
-    val someVoiceRecoding = VoiceRecording("Tom", "voicerecoding.org/id/123")
-    println(showNotification(someSms))
-    println(showNotification(someVoiceRecoding))
+//    val someSms = SMS("12345", "Are you there?")
+//    val someVoiceRecoding = VoiceRecording("Tom", "voicerecoding.org/id/123")
+//    println(showNotification(someSms))
+//    println(showNotification(someVoiceRecoding))
+    val importantPeopleInfo = Seq("867-5309", "jenny@gmail.com")
+
+    val someSms = SMS("867-5309", "Are you there?")
+    val someVoiceRecording = VoiceRecording("Tom", "voicerecording.org/id/123")
+    val importantEmail = Email("jenny@gmail.com", "Drinks tonight?", "I'm free after 5!")
+    val importantSms = SMS("867-5309", "I'm here! Where are you?")
+
+    println(showImportantNotification(importantEmail, importantPeopleInfo))
+    println(showImportantNotification(someVoiceRecording, importantPeopleInfo))
+    println(showImportantNotification(importantEmail, importantPeopleInfo))
+    println(showImportantNotification(importantSms, importantPeopleInfo))
   }
   //def main(args: Array[String]): Unit = {
   //  noData
